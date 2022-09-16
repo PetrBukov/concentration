d1ffeafeddd21901c845089e0350926b1c161c1f
Petr Bukov <pbu.vsl@icloud.com>
2022-09-16 18:26:37 +0300

    feat: add moon to the main page

--- src/pages/MainMenuPage/MainMenuPage.styles.ts
+++ src/pages/MainMenuPage/MainMenuPage.styles.ts
@@ -1,91 +1,59 @@
 import styled from '@emotion/styled';
+import { keyframes } from '@emotion/react';
+
+const moonHueRotate = keyframes`
+    0% {
+      filter: hue-rotate(307deg) brightness(0.85);
+    }
+    50% {
+      filter: hue-rotate(346deg) brightness(0.85);
+    }
+    100% {
+      filter: hue-rotate(307deg) brightness(0.85);
+    }
+`;
 
 export const MainMenuPageContainer = styled.div`
-  color: var(--yellow);
-  text-transform: uppercase;
   display: grid;
+  grid-gap: 2em;
   justify-content: center;
-  align-items: center;
   align-content: center;
-  padding-top: 100px;
 
   .main-menu {
     display: grid;
     grid-gap: 14px;
+    z-index: 3;
   }
 
   h1.main-title {
-    font-family: 'simpleStamp', sans-serif;
-    text-transform: uppercase;
-    text-align: center;
+    position: relative;
     display: grid;
     align-items: center;
+    height: 6em;
     margin: 0;
-    transform: translateY(-0.7em);
-    grid-gap: 0.05em;
+    font-family: 'simpleStamp', sans-serif;
   }
 
-  .main-title__subtitle {
-    font-size: 0.4em;
-    letter-spacing: 0.3em;
-    transform: translateY(-0.15em);
-  }
   .main-title__name {
-    transform: scale(1.1);
     display: block;
-    text-shadow: -0.04em 0.04em 0 rgba(0, 0, 0, 08);
-    -webkit-text-stroke-width: 0.002em;
-    -webkit-text-stroke-color: var(--yellow);
-    -webkit-text-stroke: 0.002em var(--yellow);
-  }
-  .main-title__name-letter {
-    font-size: 1.5em;
+    transform: translate(0.05em, 0.2em) scaleY(0.5);
+    text-shadow: -1px -1px 0 var(--yellow), 1px -1px 0 var(--yellow),
+      -1px 1px 0 var(--yellow), 1px 1px 0 var(--yellow),
+      -0.06em 0.06em 0 rgba(0, 0, 0, 08);
+    font-size: 2em;
+    letter-spacing: 0.2em;
     color: var(--red);
-    --scale: 1;
-    --rotate: 0;
-    --translateX: 0;
-    --translateY: 0;
-    --rotateX: 0deg;
-    transform-origin: bottom;
-    transform: scale(var(--scale)) rotate(var(--rotate))
-      translateX(var(--translateX)) translateY(var(--translateY))
-      rotateX(var(--rotateX));
-    display: inline-block;
-    line-height: 1;
-    transition: transform 0.3s;
+    z-index: 2;
+  }
 
-    &.M1 {
-      --scale: 1;
-      --translateX: -1.04em;
-      --translateY: -0.2em;
-    }
-    &.E1 {
-      --scale: 1.2;
-      --translateX: -0.74em;
-      --translateY: -0.13em;
-    }
-    &.M2 {
-      --scale: 1.5;
-      --translateX: -0.39em;
-      --translateY: -0.06em;
-    }
-    &.O1 {
-      --scale: 2;
-    }
-    &.O2 {
-      --scale: 1.5;
-      --translateX: 0.35em;
-      --translateY: -0.06em;
-    }
-    &.R {
-      --scale: 1.2;
-      --translateX: 0.7em;
-      --translateY: -0.13em;
-    }
-    &.E2 {
-      --scale: 1;
-      --translateX: 1em;
-      --translateY: -0.2em;
-    }
+  .main-title__moon {
+    position: absolute;
+    left: 50%;
+    top: 62%;
+    transform: translate(-50%, -50%) scale(2);
+    z-index: 1;
+    font-size: 3em;
+    filter: hue-rotate(307deg) brightness(0.85);
+    animation: ${moonHueRotate} 20s infinite linear;
   }
 `;
--- src/pages/MainMenuPage/MainMenuPage.tsx
+++ src/pages/MainMenuPage/MainMenuPage.tsx
@@ -17,16 +17,8 @@ export const MainMenuPage: React.FC = () => {
   return (
     <MainMenuPageContainer>
       <h1 className="main-title">
-        <div className="main-title__name">
-          <span className="main-title__name-letter M1">M</span>
-          <span className="main-title__name-letter E1">E</span>
-          <span className="main-title__name-letter M2">M</span>
-          <span className="main-title__name-letter O1">Ö</span>
-          <span className="main-title__name-letter O2">R</span>
-          <span className="main-title__name-letter R">I</span>
-          <span className="main-title__name-letter E2">A</span>
-        </div>
-        <div className="main-title__subtitle">let's train your brain</div>
+        <div className="main-title__moon">🌕</div>
+        <div className="main-title__name">MEMÖRIA</div>
       </h1>
       <div className="main-menu">
         <ButtonLink
